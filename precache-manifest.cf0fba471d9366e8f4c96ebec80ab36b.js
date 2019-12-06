3534aae6cd6bea3b990939a0d701b85e5785aa3b
cs-cognitome <gaya.scienza.v@gmail.com>
2019-12-06 19:08:14 +0200

    Updates

---
+++ precache-manifest.cf0fba471d9366e8f4c96ebec80ab36b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db9c5eac3a7bf512e4c5dc86409d68a",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "a5a3d65a2311654d6aed",
+    "url": "/react_tabs/static/css/main.cb6aa723.chunk.css"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/react_tabs/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "a5a3d65a2311654d6aed",
+    "url": "/react_tabs/static/js/main.671f6650.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  },
+  {
+    "revision": "0c1defc706f7ff15eaede7aa4ac42768",
+    "url": "/react_tabs/static/media/alice_bgr.0c1defc7.png"
+  }
+]);
